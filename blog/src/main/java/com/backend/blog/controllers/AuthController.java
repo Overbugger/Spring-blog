faa38c5ec77bf8e9d7d402a0d76dd515521e9233
overbugger <okorosamuelchika@gmail.com>
2025-05-25 20:30:48 +0200

    implement auth filter and service

---
+++ blog/src/main/java/com/backend/blog/controllers/AuthController.java
@@ -0,0 +1,38 @@
+package com.backend.blog.controllers;
+
+import com.backend.blog.dtos.AuthResponse;
+import com.backend.blog.dtos.LoginRequest;
+import com.backend.blog.services.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping
+    public ResponseEntity<AuthResponse> login(
+            @RequestBody LoginRequest loginRequest
+            ){
+        UserDetails userDetails = authService.authenticate(
+                loginRequest.getEmail(),
+                loginRequest.getPassword()
+        );
+
+        String tokenValue = authService.generateToken(userDetails);
+        AuthResponse response = AuthResponse.builder()
+                .token(tokenValue)
+                .expiresIn(86400)
+                .build();
+
+        return ResponseEntity.ok(response);
+    }
+
+}
---
+++ blog/src/main/java/com/backend/blog/services/AuthService.java
@@ -0,0 +1,10 @@
+package com.backend.blog.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+public interface AuthService {
+    UserDetails authenticate(String email, String password);
+    String generateToken(UserDetails userDetails);
+    UserDetails validateToken(String token);
+}
