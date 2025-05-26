4856e403719d6dc709bb1e1494e6c17e0655a0c9
overbugger <okorosamuelchika@gmail.com>
2025-05-26 08:44:09 +0200

    complete auth implementation

--- blog/src/main/java/com/backend/blog/config/SecurityConfig.java
+++ blog/src/main/java/com/backend/blog/config/SecurityConfig.java
@@ -1,5 +1,11 @@
 package com.backend.blog.config;
 
+import com.backend.blog.models.User;
+import com.backend.blog.repo.UserRepo;
+import com.backend.blog.security.BlogUserDetails;
+import com.backend.blog.security.BlogUserDetailsService;
+import com.backend.blog.security.JwtAuthFilter;
+import com.backend.blog.services.AuthService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,17 +13,28 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public JwtAuthFilter jwtAuthFilter(AuthService authService) {
+        return new JwtAuthFilter(authService);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            JwtAuthFilter jwtAuthFilter
+    ) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/post/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/categories/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/tags/**").permitAll()
@@ -26,9 +43,28 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sessions ->
                         sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                ).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepo userRepo) {
+//        BlogUserDetailsService blogUserDetailsService = new BlogUserDetailsService(userRepo);
+//        String email = "user@email.com";
+//
+//        userRepo.findByEmail(email).orElseGet(() -> {
+//            User newUser = User.builder()
+//                    .name("Test user")
+//                    .email(email)
+//                    .password(passwordEncoder().encode("password"))
+//                    .build();
+//            return userRepo.save(newUser);
+//        });
+//
+//        return blogUserDetailsService;
+
+       return new BlogUserDetailsService(userRepo);
     }
 
     @Bean
--- blog/src/main/java/com/backend/blog/services/impl/AuthServiceImpl.java
+++ blog/src/main/java/com/backend/blog/services/impl/AuthServiceImpl.java
@@ -1,5 +1,7 @@
 package com.backend.blog.services.impl;
 
+import com.backend.blog.models.User;
+import com.backend.blog.repo.UserRepo;
 import com.backend.blog.services.AuthService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
@@ -9,32 +11,59 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
     private final AuthenticationManager authenticationManager;
     private final UserDetailsService userDetailsService;
+    private final UserRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
 
     @Value("${jwt.secret}")
     private String secretKey;
 
-    private final Long jwtExpiryMs = 86400000L;
+    private final long jwtExpiryMs = 86400000L;
 
     @Override
     public UserDetails authenticate(String email, String password) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(email, password)
-        );
-        return userDetailsService.loadUserByUsername(email);
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(email, password)
+            );
+            return userDetailsService.loadUserByUsername(email);
+        } catch (AuthenticationException e) {
+            Optional<User> foundUser = userRepo.findByEmail(email);
+
+            if (foundUser.isEmpty()) {
+                String newUsername = email.substring(0, email.indexOf("@"));
+
+                User newUser = User.builder()
+                        .name(newUsername)
+                        .email(email)
+                        .password(passwordEncoder.encode(password))
+                        .build();
+                userRepo.save(newUser);
+
+                authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(email, password)
+                );
+                return userDetailsService.loadUserByUsername(email);
+            } else {
+                throw e;
+            }
+        }
     }
 
     @Override
@@ -45,7 +74,7 @@ public class AuthServiceImpl implements AuthService {
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + jwtExpiryMs))
-                .signWith(getSigningKey(), SignatureAlgorithm.ES256)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
